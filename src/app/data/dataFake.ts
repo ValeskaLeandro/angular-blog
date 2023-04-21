8d04d5587ecaef75e3f06b0da815ecbafd37732b
Valeska Leandro <valeskaleandro@gmail.com>
2023-04-21 18:49:11 -0300

    Content created dinamyc

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "photo": "https://cabanadoleitor.com.br/wp-content/uploads/2022/12/Robert-Downey-Jr.-Homem-de-Ferro-cabana-1536x864.webp",
+        "title": "Tudo sobre o Homem de Ferro!",
+        "description": "O Homem de Ferro (Iron Man) foi um personagem dos quadrinhos publicados pela Marvel Comics. Sua verdadeira identidade é o empresário e bilionário Tony Stark, que usou armaduras de alta tecnologia no combate ao crime. Foi criado em1963 pelo escritor Stan Lee, o roteirista Larry Lieber e os desenhistas Jack Kirby e Don Heck. Stan Lee aceitou o desafio de fazer um personagem ser odiado e depois amado pelo público, criando um dos super-heróis mais marcantes de todos os tempos. "
+    },
+    {
+        "id": "2",
+        "photo": "https://i.ytimg.com/vi/zl29F2c1Vf0/maxresdefault.jpg",
+        "title": "Saiba tudo sobre Gal Gadot, a Mulher Maravilha!",
+        "description": "Gal Gadot-Varsano[1] (em hebraico: גל גדות; Petah Tikva, 30 de abril de 1985) é uma atriz e modelo israelense, conhecida principalmente pelos papéis de Gisele na franquia The Fast and the Furious[2] e Diana Prince em Mulher-Maravilha no Universo Estendido DC.[3]        Gal Gadot nasceu em Petah Tikva e foi criada na cidade vizinha de Rosh HaAyin, no Distrito Central de Israel.[4] Em hebraico, seu primeiro nome significa onda e o segundo margens dos rios.[5] Seus pais são Irit (nascida Weiss), uma professora, e Michael Gadot, um engenheiro."
+    },
+    {
+        "id": "3",
+        "photo": "https://gkpb.com.br/wp-content/uploads/2022/06/fagWFrNEGrM2p3DCOwgrO5fBmUn-scaled-e1655148584687.jpg",
+        "title": "HOMEM-ARANHA: SEM VOLTA PARA CASA",
+        "description": "Em Homem-Aranha: Sem Volta para Casa, Peter Parker precisará lidar com as consequências da sua identidade como o herói mais querido do mundo após ter sido revelada pela reportagem do Clarim Diário, com uma gravação feita por Mysterio no filme anterior. Incapaz de separar sua vida normal das aventuras de ser um super-herói, além de ter sua reputação arruinada por acharem que foi ele quem matou Mysterio e pondo em risco seus entes mais queridos, Parker pede ao Doutor Estranho para que todos esqueçam sua verdadeira identidade."
+    },
+    {
+        "id": "4",
+        "photo": "https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2022/10/rev-1-BAD-T2-009r_High_Res_JPEG.jpeg?w=876&h=484&crop",
+        "title": "The Rock estrela Adão Negro, novo filme da DC.",
+        "description": "Um dos maiores lançamentos da DC Comics este ano, o anti-herói Adão Negro (2022) finalmente terá detalhes de sua história revelados. Protagonizado por Dwayne Johnson, conhecido também como The Rock, o filme promete inaugurar nova ... - Leia mais em https://rollingstone.uol.com.br/noticia/adao-negro-tudo-o-que-sabemos-sobre-novo-filme-da-dc-com-the-rock/?utm_source=site&utm_medium=txt&utm_campaign=copypaste"
+    },
+]
